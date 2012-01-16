011e1d8649e54923b2b9e06ede0b5867cb59d546
Chris Bowdon <c.bowdon@gmail.com>
2012-01-16 18:38:12 +0800

    ConvertComics has been spec'd

---
+++ TestSeqDLLib/ComicConvert.cs
@@ -0,0 +1,34 @@
+using System;
+using NUnit.Framework;
+
+namespace TestSeqDLLib
+{
+	[TestFixture()]
+	public class ComicConvert
+	{
+		[Test()]
+		public void ImgsToCbz ()
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[Test()]
+		public void CbzToImgs ()
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[Test()]
+		public void ImgsToPdf ()
+		{
+			throw new NotImplementedException ();
+		}
+		
+		[Test()]
+		public void PdfToImgs ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
+
